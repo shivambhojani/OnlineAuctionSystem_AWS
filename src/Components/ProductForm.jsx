1f2d63a672924ea75aaf44a33a9301a9e5ad5be7
Bhavesh Lalwani <bhavesh.lalwani@dal.ca>
2022-07-13 18:50:56 -0300

    fetching products from db

--- src/Components/ProductForm.jsx
+++ src/Components/ProductForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -12,124 +12,125 @@ import axios from 'axios';
 
 
 export default function ProductForm() {
+  const baseUrl = "https://dec7ccapye.execute-api.us-east-1.amazonaws.com/prod/";
 
-    const [productName, setProductName] = useState("");
-    const [productDescription, setProductDescription] = useState("");
-    const [price, setPrice] = useState("");
-    const [image, setImage] = useState("");
-  
-    const navigate = useNavigate();
-  
-    const handleSubmit = (e) => {
-        if(!image) {
-            alert("Please upload image");
-            return;
-        }
-        if(!productName || !productDescription || !price) {
-            alert("Please fill all the requried fields")
-        } 
-        const data = {
-            "productName": productName,
-            "description": productDescription,
-            "price": price,
-            "imgUrl": "https://5409-assignement-2.s3.amazonaws.com/cancel+tickets+click+stream.drawio.png"
-        }
-        axios.post("https://24rgwa3gca.execute-api.us-east-1.amazonaws.com/prod/product", data)
-        .then(res => { 
-            console.log(res.statusText)
-        })
-    };
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
 
-    const imageHandler = (event) => {
-       setImage(event.target.files[0])
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    if (!image) {
+      alert("Please upload image");
+      return;
     }
-
-    const fileUploadHandler = () => {
-        const fd = new FormData();
-        fd.append('image', image, image.name)
-        // axios.post('url', fd).then(res => {
-        //     console.log(res);
-        // })
+    if (!productName || !productDescription || !price) {
+      alert("Please fill all the requried fields")
     }
-    
-    const theme = createTheme();
-  
-    return (
-      <ThemeProvider theme={theme}>
-        <Grid container component="main" sx={{ height: '100vh' }}>
-          <CssBaseline />
-          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <Box
-              sx={{
-                my: 8,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              
-              <Typography component="h1" variant="h5">
-                Add Product
-              </Typography>
-             
-              <Box component="form" noValidate  sx={{ mt: 1 }}>
-                {/* Name */}
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={productName}
-                  onChange={(e) => setProductName(e.target.value)}
-                  id="email"
-                  name="Product Name"
-                  label="Product name"
-                  autoFocus
-                />
-                {/* Description */}
-                <TextField
-                    label="Product Description"
-                    value={productDescription}
-                    multiline
-                    fullWidth
-                    required
-                    rows={2}
-                    onChange={(e) => setProductDescription(e.target.value)}
-                />
-                {/* price */}
-                <TextField
-                  margin="normal"
-                  required
-                  type="number"
-                  fullWidth
-                  value={price}
-                  InputProps={{ inputProps: { min: 1 } }}
-                  onChange={(e) => setPrice(e.target.value)}
-                  name="price"
-                  label="price"
-                />
-                {/* Image */}
-                Add Image: 
-                <input
-                    accept="image/*"
-                    multiple
-                    type="file"
-                    onChange={imageHandler}
-                />
-               
-                <Button
-                  type="button"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                  onClick={handleSubmit}
-                >
-                  Add Product
-                </Button>
-              </Box>
-            </Box>
-          </Grid>
-        </Grid>
-      </ThemeProvider>
-    );
+    const data = {
+      "name": productName,
+      "description": productDescription,
+      "price": price,
+      "imgUrl": "https://5409-assignement-2.s3.amazonaws.com/cancel+tickets+click+stream.drawio.png"
+    }
+    axios.post(baseUrl + "product", data)
+      .then(res => {
+        console.log(res.statusText)
+      })
+  };
+
+  const imageHandler = (event) => {
+    setImage(event.target.files[0])
   }
+
+  const fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', image, image.name)
+    // axios.post('url', fd).then(res => {
+    //     console.log(res);
+    // })
+  }
+
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+
+            <Typography component="h1" variant="h5">
+              Add Product
+            </Typography>
+
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              {/* Name */}
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+                id="email"
+                name="Product Name"
+                label="Product name"
+                autoFocus
+              />
+              {/* Description */}
+              <TextField
+                label="Product Description"
+                value={productDescription}
+                multiline
+                fullWidth
+                required
+                rows={2}
+                onChange={(e) => setProductDescription(e.target.value)}
+              />
+              {/* price */}
+              <TextField
+                margin="normal"
+                required
+                type="number"
+                fullWidth
+                value={price}
+                InputProps={{ inputProps: { min: 1 } }}
+                onChange={(e) => setPrice(e.target.value)}
+                name="price"
+                label="price"
+              />
+              {/* Image */}
+              Add Image:
+              <input
+                accept="image/*"
+                multiple
+                type="file"
+                onChange={imageHandler}
+              />
+
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleSubmit}
+              >
+                Add Product
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
